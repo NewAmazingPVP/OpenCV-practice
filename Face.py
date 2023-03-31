a97a4f216a4b810556475ebd3b96294a7319ad67
NewAmazingPVP <gamingofpat@gmail.com>
2023-03-31 12:18:29 -0400

    Have added approximate degrees the object is facing away from camera
    Have added approximate degrees the object is facing away from camera

--- Face.py
+++ Face.py
@@ -1,3 +1,4 @@
+import math
 import cv2
 
 # Load the face detection classifier
@@ -27,15 +28,21 @@ while True:
         distance = (2 * focal_length * 20) / face_width  # Distance in centimeters (20 is the size of the face in centimeters)
         distance = round(distance / 100, 2)  # Convert to meters and round to two decimal places
 
-        # Display the distance next to the face
-        cv2.putText(frame, f"{distance} m", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        # Calculate the angle of the face relative to the camera
+        fov = 78 # in degrees
+        angle = math.atan(((x + w/2) - frame.shape[1]/2) / (0.5 * frame.shape[1] / math.tan(0.5 * math.radians(fov))))
+        angle = round(math.degrees(angle), 2)
+
+        # Display the distance and angle next to the face
+        cv2.putText(frame, f"{angle} deg", (x+w-60, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.putText(frame, f"{distance} m", (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     # Show the frame
     cv2.imshow('frame', frame)
     
     cv2.waitKey(1)
 
-    # Exit if the user presses the 'q' key
+    # Exit if the user presses the 'e' key
     if cv2.waitKey(1) & 0xFF == ord('e'):
         break
 
