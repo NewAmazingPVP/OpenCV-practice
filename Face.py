4935301be7723ca5152c534d1fccd2e8e76f0d82
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-06-16 22:46:18 -0400

    Alllow many more object detections from new library
    Alllow many more object detections from new library

--- Face.py
+++ Face.py
@@ -1,35 +1,29 @@
-import math
+import asone
+from asone import utils
+from asone import ASOne
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+detector = ASOne(detector=asone.YOLOV8L_PYTORCH , use_cuda=True) # Set use_cuda to False for cpu
+# for list https://github.com/augmentedstartups/AS-One/blob/main/asone/linux/Instructions/Benchmarking.md
+# best peformance to quality ratio: YOLOV6S_REPOPT_PYTORCH
+
 
 cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        focal_length = 600  # Focal length of the camera in pixels **NOTE: This is approximate**
-        face_width = w  
-        distance = (2 * focal_length * 20) / face_width  # (20 is the size of the face in centimeters)
-        distance = round(distance / 100, 2)
-
-        fov = 78 # in degrees
-        angle = math.atan(((x + w/2) - frame.shape[1]/2) / (0.5 * frame.shape[1] / math.tan(0.5 * math.radians(fov))))
-        angle = round(math.degrees(angle), 2)
-
-        cv2.putText(frame, f"{angle} deg", (x+w-60, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.putText(frame, f"{distance} m", (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-    cv2.imshow('frame', frame)
-    
-    cv2.waitKey(1)
-
-    if cv2.waitKey(1) & 0xFF == ord('e'):
+    _, frame = cap.read()
+    if not _:
         break
+    
+    dets, img_info = detector.detect(frame)
 
-cap.release()
-cv2.destroyAllWindows()
+    bbox_xyxy = dets[:, :4]
+    scores = dets[:, 4]
+    class_ids = dets[:, 5]
+
+    frame = utils.draw_boxes(frame, bbox_xyxy, class_ids=class_ids)
+
+    cv2.imshow('result', frame)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
