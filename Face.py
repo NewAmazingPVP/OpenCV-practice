bb8b6f238f7227a404e0b4d82c438c7475fc656f
NewAmazingPVP <gamingofpat@gmail.com>
2023-06-17 16:41:11 -0400

    1080p display with fps

--- Face.py
+++ Face.py
@@ -2,19 +2,31 @@ import asone
 from asone import utils
 from asone import ASOne
 import cv2
+import time
 
 detector = ASOne(detector=asone.YOLOV8L_PYTORCH , use_cuda=True) # Set use_cuda to False for cpu
 # for list https://github.com/augmentedstartups/AS-One/blob/main/asone/linux/Instructions/Benchmarking.md
 # best peformance to quality ratio: YOLOV6S_REPOPT_PYTORCH
 
-
 cap = cv2.VideoCapture(0)
 
+# Set the camera's resolution
+width = 1920
+height = 1080
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+# Create a named window and set it to fullscreen
+window_name = "result"
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 while True:
+    start_time = time.time()
     _, frame = cap.read()
     if not _:
         break
-    
+
     dets, img_info = detector.detect(frame)
 
     bbox_xyxy = dets[:, :4]
@@ -23,7 +35,12 @@ while True:
 
     frame = utils.draw_boxes(frame, bbox_xyxy, class_ids=class_ids)
 
-    cv2.imshow('result', frame)
+    # Calculate FPS and display it on the frame
+    end_time = time.time()
+    fps = 1 / (end_time - start_time)
+    cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow(window_name, frame)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
